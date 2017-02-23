ca6acb2f7e635a8f376dfb20f917aeadbb2e7d33
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-02-23 17:49:36 -0600

    Create mod.py

---
+++ modules/mod.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class Mod:
+    def __init__(self, bot):
+        self.bot = bot
+        
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member, *, reason: str=None):
+    """Kicks a user from your server."""
+    author = ctx.message.author
+    server = author.server
+    try:
+       await bot.send_message(user, "You have been kicked from {}!\nReason: {}".format(author.server, reason))
+       await bot.kick(user)
+    except:
+       await bot.say("I cannot kick that user, or I don't have `kick_members` permission.")
+       
+def setup(bot):
+    n = Mod(bot)
+    bot.add_cog(n)
