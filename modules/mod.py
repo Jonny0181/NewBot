e5d9a2d6e133af1cddd334174b9fe46757fdccbd
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-02-23 21:06:03 -0600

    Update mod.py

--- modules/mod.py
+++ modules/mod.py
@@ -5,17 +5,17 @@ class Mod:
     def __init__(self, bot):
         self.bot = bot
         
-@bot.command(pass_context=True)
-@bot.has_role('Bot Commander')
+@commands.command(pass_context=True)
+@commmands.has_role('Bot Commander')
 async def kick(ctx, user: discord.Member, *, reason: str=None):
     """Kicks a user from your server."""
     author = ctx.message.author
     server = author.server
     try:
-       await bot.send_message(user, "You have been kicked from {}!\nReason: {}".format(author.server, reason))
-       await bot.kick(user)
+       await self.bot.send_message(user, "You have been kicked from {}!\nReason: {}".format(author.server, reason))
+       await self.bot.kick(user)
     except:
-       await bot.say("I cannot kick that user, or I don't have `kick_members` permission.")
+       await self.bot.say("I cannot kick that user, or I don't have `kick_members` permission.")
        
 def setup(bot):
     n = Mod(bot)
