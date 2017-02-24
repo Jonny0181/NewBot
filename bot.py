d66a3ab7b3b32bfa8f77d7c876e7a765dfe390d6
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-02-23 20:52:37 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,7 +116,7 @@ async def setname(ctx, *, name: str):
 bot.add_cog(Default(bot))
 loop = asyncio.get_event_loop()
 try:
-    loop.run_until_complete(bot.login(""))
+    loop.run_until_complete(bot.login("Mjg0NTE3NzQ2MzMzNDUwMjUx.C5ExUA.ww2YQX7pEri504iXJ7rOiQHCvB0"))
     loop.run_until_complete(bot.connect())
 except Exception:
     loop.run_until_complete(os.system("main.py"))
