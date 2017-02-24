5b45fee63c1566a731e5d182e80e4939f4684898
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-02-23 21:06:53 -0600

    Update mod.py

--- modules/mod.py
+++ modules/mod.py
@@ -6,7 +6,7 @@ class Mod:
         self.bot = bot
         
 @commands.command(pass_context=True)
-@commmands.has_role('Bot Commander')
+@commands.has_role('Bot Commander')
 async def kick(ctx, user: discord.Member, *, reason: str=None):
     """Kicks a user from your server."""
     author = ctx.message.author
